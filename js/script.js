9d87858d891e29e87350a6e411c7bb3844ef8fe2
malinhodev <mcterasgo@gmail.com>
2022-07-14 12:48:33 -0300

    versão 1.2.0

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let buttons = document.querySelectorAll("#buttons-container button");
+let buttons = document.querySelectorAll("#buttons-container .btn");
 let messageContainer = document.querySelector("#message");
 let messageText = document.querySelector("#message p");
 let secondPlayer;
@@ -33,6 +33,7 @@ for(let i = 0; i < boxes.length; i++){
         }
     });
 }
+
 //2play ou IA
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", function(){
